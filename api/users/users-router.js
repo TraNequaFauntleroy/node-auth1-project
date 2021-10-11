01899a527e668b3bfdfce7bdace4efce6897b07d
TraNequaFauntleroy <shecodes@nenethecreative.com>
2021-10-11 15:41:31 -0400

    get ep for users is wired up

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,4 +1,8 @@
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
+const router = require('express').Router()
+
+// const { restricted } = require('../auth/auth-middleware')
+// const Users = require('./users-model')
 
 
 /**
@@ -24,5 +28,13 @@
   }
  */
 
+router.get('/', (req, res, next) => {
+  // Users.find()
+  //   .then(users => {
+  //     res.status(200).json(users)
+  //   })
+  //   .catch(next)
+  res.json('users')
+})
 
-// Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router
