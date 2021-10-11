24c917ec6695cbaf7ec7affb1222baa078af257f
TraNequaFauntleroy <shecodes@nenethecreative.com>
2021-10-11 15:54:29 -0400

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -6,16 +6,16 @@
     "message": "You shall not pass!"
   }
 */
-// function restricted(req, res, next) {
-//   if (req.session.user) {
-//     next()
-//   } else {
-//     next({
-//       status: 401,
-//       message: 'invalid credentials'
-//     })
-//   }
-// }
+function restricted(req, res, next) {
+  if (req.session.user) {
+    next()
+  } else {
+    next({
+      status: 401,
+      message: 'invalid credentials'
+    })
+  }
+}
 
 /*
   If the username in req.body already exists in the database
@@ -25,8 +25,8 @@
     "message": "Username taken"
   }
 */
-function checkUsernameFree() {
-
+function checkUsernameFree(req, res, next) {
+  next()
 }
 
 /*
@@ -37,8 +37,8 @@ function checkUsernameFree() {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists() {
-
+function checkUsernameExists(req, res, next) {
+  next()
 }
 
 /*
@@ -49,8 +49,8 @@ function checkUsernameExists() {
     "message": "Password must be longer than 3 chars"
   }
 */
-function checkPasswordLength() {
-
+function checkPasswordLength(req, res, next) {
+  next()
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
